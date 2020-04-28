bb5f4c290015fc8dfdb09fdf65b35c7defcf189a
Deval Shilu <Deval.Shilu@capita.co.uk>
2020-04-29 01:57:29 +0530

    bug fix

--- src/db/db.js
+++ src/db/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://deval:Deval111@cluster0-90tef.mongodb.net/test?retryWrites=true&w=majority', {
+mongoose.connect(process.env.MONGODB_URL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useFindAndModify: true
